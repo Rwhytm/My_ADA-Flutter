bf51b4e80fb729c3bb6ff0811aad729ce39ce0d3
Rwhytm <rajuwahyudi842@gmail.com>
2022-06-15 01:54:33 +0700

    font and textstyle

---
+++ buku_kia/lib/themes/font.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:buku_kia/themes/color.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle judulHitamPudar = GoogleFonts.poppins(
+  color: subtitleHitam,
+  fontSize: 15,
+  fontWeight: FontWeight.w900,
+);
+
+TextStyle judulAppBAr = GoogleFonts.poppins(
+  color: putih,
+  fontSize: 20,
+  fontWeight: FontWeight.w900,
+);
+
+TextStyle judulBiru = GoogleFonts.poppins(
+  color: subtitleBiru,
+  fontSize: 10,
+  fontWeight: FontWeight.w900,
+);
