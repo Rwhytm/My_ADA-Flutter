89acaadc5968f8ef88b68b8f320462124d61d1cf
Rwhytm <rajuwahyudi842@gmail.com>
2022-06-15 01:43:48 +0700

    add color of app

--- buku_kia/lib/pages/splash_screen.dart
+++ buku_kia/lib/pages/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -6,7 +8,23 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/images/sampul.png'),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "BUKU KIA",
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            Image.asset(
+              'assets/images/sampul.png',
+              width: 300,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ buku_kia/lib/themes/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundPink = Color(0xffDB65B8);
+const Color unguColor = Color(0xff8428A4);
+const Color bundaranUngu = Color(0xff94B2EB);
+const Color subtitleBiru = Color(0xff50ACDF);
+const Color subtitleHitam = Color(0xff56595A);
+const Color hijauMuda = Color(0xffAFF198);
+const Color hijauTua = Color(0xff30C13E);
+const Color merahBox = Color(0xffFD4343);
