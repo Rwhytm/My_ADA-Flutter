a4b2c2a14c8f874dd58e0efc45fdbcb171b08a75
Rwhytm <rajuwahyudi842@gmail.com>
2022-06-30 14:33:34 +0700

    revision 1

--- buku_kia/lib/pages/daftar_isi.dart
+++ buku_kia/lib/pages/daftar_isi.dart
@@ -49,206 +49,225 @@ class DaftarIsi extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(25),
         color: const Color(0xffFF93DF),
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Center(
-                    child: GestureDetector(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Registrasi(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: catatanColor,
+                            borderRadius: BorderRadius.circular(27),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Image.asset(
+                                'assets/images/form.png',
+                                width: 100,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Flexible(
+                                child: Text(
+                                  "Registrasi",
+                                  textAlign: TextAlign.center,
+                                  style: daftarIsi,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Registrasi(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: catatanColor,
+                            borderRadius: BorderRadius.circular(27),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Image.asset(
+                                'assets/images/form.png',
+                                width: 100,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Flexible(
+                                child: Text(
+                                  "Tim Peneliti",
+                                  textAlign: TextAlign.center,
+                                  style: daftarIsi,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => Registrasi(),
-                          ),
+                              builder: (context) => const IbuHamil1()),
                         );
                       },
                       child: Container(
                         width: 150,
                         height: 150,
-                        padding: const EdgeInsets.all(5),
                         decoration: BoxDecoration(
-                          color: catatanColor,
+                          color: ibuHamilColor,
                           borderRadius: BorderRadius.circular(27),
                         ),
+                        padding: const EdgeInsets.all(5),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            Image.asset(
-                              'assets/images/form.png',
-                              width: 100,
+                            Text(
+                              "Ibu Hamil",
+                              style: daftarIsi,
                             ),
                             const SizedBox(
                               height: 10,
                             ),
-                            Flexible(
-                              child: Text(
-                                "Registrasi",
-                                textAlign: TextAlign.center,
-                                style: daftarIsi,
-                              ),
+                            Image.asset(
+                              'assets/images/cium.png',
+                              width: 100,
                             ),
                           ],
                         ),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const IbuHamil1()),
-                      );
-                    },
-                    child: Container(
-                      width: 150,
-                      height: 150,
-                      decoration: BoxDecoration(
-                        color: ibuHamilColor,
-                        borderRadius: BorderRadius.circular(27),
-                      ),
-                      padding: const EdgeInsets.all(5),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "Ibu Hamil",
-                            style: daftarIsi,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Image.asset(
-                            'assets/images/cium.png',
-                            width: 100,
-                          ),
-                        ],
-                      ),
+                    const SizedBox(
+                      height: 30,
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const IbuNifas1()),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(27),
-                        color: ibuNifasColor,
-                      ),
-                      width: 150,
-                      height: 150,
-                      padding: const EdgeInsets.all(5),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Image.asset(
-                            'assets/images/makan.png',
-                            width: 100,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "Ibu Nifas",
-                            style: daftarIsi,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const IbuBersalin1()),
-                      );
-                    },
-                    child: Container(
-                      width: 150,
-                      height: 150,
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: ibuBersalinColor,
-                        borderRadius: BorderRadius.circular(27),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Image.asset(
-                            'assets/images/bersalin.png',
-                            width: 100,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "Ibu Bersalin",
-                            style: daftarIsi,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const KeluargaBerencana()),
-                      );
-                    },
-                    child: Container(
-                      width: 150,
-                      height: 150,
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: keluargaBerencanaColor,
-                        borderRadius: BorderRadius.circular(27),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Flexible(
-                            child: Text(
-                              "Keluarga Berencana",
-                              style: daftarIsi,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Image.asset(
-                            'assets/images/obat.png',
-                            width: 100,
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: GestureDetector(
+                    GestureDetector(
                       onTap: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => const Menu()),
+                          MaterialPageRoute(
+                              builder: (context) => const IbuNifas1()),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(27),
+                          color: ibuNifasColor,
+                        ),
+                        width: 150,
+                        height: 150,
+                        padding: const EdgeInsets.all(5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/makan.png',
+                              width: 100,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Ibu Nifas",
+                              style: daftarIsi,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Menu()),
+                          );
+                        },
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: catatanColor,
+                            borderRadius: BorderRadius.circular(27),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Image.asset(
+                                'assets/images/form.png',
+                                width: 100,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Flexible(
+                                child: Text(
+                                  "Catatan Kesehatan Ibu Hamil",
+                                  textAlign: TextAlign.center,
+                                  style: daftarIsi,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const IbuBersalin1()),
                         );
                       },
                       child: Container(
@@ -256,34 +275,74 @@ class DaftarIsi extends StatelessWidget {
                         height: 150,
                         padding: const EdgeInsets.all(5),
                         decoration: BoxDecoration(
-                          color: catatanColor,
+                          color: ibuBersalinColor,
                           borderRadius: BorderRadius.circular(27),
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Image.asset(
-                              'assets/images/form.png',
+                              'assets/images/bersalin.png',
                               width: 100,
                             ),
                             const SizedBox(
                               height: 10,
                             ),
-                            Flexible(
-                              child: Text(
-                                "Catatan Kesehatan Ibu Hamil",
-                                textAlign: TextAlign.center,
-                                style: daftarIsi,
-                              ),
+                            Text(
+                              "Ibu Bersalin",
+                              style: daftarIsi,
                             ),
                           ],
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const KeluargaBerencana()),
+                        );
+                      },
+                      child: Container(
+                        width: 150,
+                        height: 150,
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: keluargaBerencanaColor,
+                          borderRadius: BorderRadius.circular(27),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                "Keluarga Berencana",
+                                style: daftarIsi,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Image.asset(
+                              'assets/images/obat.png',
+                              width: 100,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
